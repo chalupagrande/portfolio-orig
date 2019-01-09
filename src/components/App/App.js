aab46a11f2cccaced973936aa71f24717f714c74
Jamie Skinner <skinner520@gmail.com>
2019-01-08 18:09:31 -0600

    fix: remove reference to house in react app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from '../Home/Home'
 import About from '../About/About'
-import House from '../../pages/House/House'
 import NoMatch from '../NoMatch/NoMatch'
 import './App.css'
 
@@ -13,7 +12,6 @@ class App extends Component {
         <Switch>
           <Route path='/' exact component={About}/>
           <Route path='/about' exact component={Home}/>
-          <Route path='/house' exact component={House}/>
           <Route component={NoMatch}/>
         </Switch>
       </BrowserRouter>
